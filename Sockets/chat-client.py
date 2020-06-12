bf3f2fa1b47af589493527709434ab3de7f19083
UwePabst91052 <upabst55@gmail.com>
2020-06-12 23:43:17 +0200

    Extended solution using selectors

--- Sockets/chat-client.py
+++ Sockets/chat-client.py
@@ -1,15 +1,68 @@
 import socket
+import selectors
+import types
 
 HOST = "127.0.0.1"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        try:
-            message = bytes(input("Nachricht>"), encoding='utf-8')
-        except KeyboardInterrupt:
-            s.sendall(b'exit')
-            break
-        s.sendall(message)
+sel = selectors.DefaultSelector()
 
+
+def start_connection(host, port):
+    global send_data
+    server_addr = (host, port)
+    print("starting connection to", server_addr)
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.setblocking(False)
+    lsock.connect_ex(server_addr)
+    levents = selectors.EVENT_WRITE
+    send_data = types.SimpleNamespace(
+        msg_total=0,
+        recv_total=0,
+        message=None,
+        outb=b"",
+    )
+    sel.register(lsock, levents, data=send_data)
+    return lsock
+
+
+def prepare_send_data(sock, message):
+    global send_data
+    sel.unregister(sock)
+    send_data = types.SimpleNamespace(
+        msg_total=len(message),
+        message=bytes(message, encoding='utf-8'),
+        outb=b"",
+    )
+    sel.register(sock, selectors.EVENT_WRITE, data=send_data)
+
+
+def service_connection(key, mask):
+    lsock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.message:
+            data.outb = data.message
+        if data.outb:
+            print("sending", repr(data.outb))
+            sent = lsock.send(data.outb)
+            data.outb = data.outb[sent:]
+
+
+send_data = None
+gsock = start_connection(HOST, int(PORT))
+
+run = True
+while run:
+    try:
+        text = input("Nachricht>")
+    except KeyboardInterrupt:
+        text = "exit"
+        run = False
+    prepare_send_data(gsock, text)
+    events = sel.select(timeout=1)
+    if events:
+        for l_key, l_mask in events:
+            service_connection(l_key, l_mask)
+sel.unregister(gsock)
+sel.close()
--- Sockets/chat-server.py
+++ Sockets/chat-server.py
@@ -1,24 +1,57 @@
 import socket
 import time
+import selectors
+import types
 
 HOST = "127.0.0.1"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by", addr)
-        try:
-            while True:
-                data = conn.recv(1024)
-                if data:
-                    print("Received", repr(data))
-                    if data == b'exit':
-                        break
-                else:
-                    time.sleep(0.5)
-        except KeyboardInterrupt:
-            pass
 
+sel = selectors.DefaultSelector()
+
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print("accepted connection from", addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ
+    sel.register(conn, events, data=data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.inb = recv_data
+            if data.inb == b'exit':
+                print("closing connection to", data.addr)
+                sel.unregister(sock)
+                sock.close()
+                return -1
+            else:
+                print("Received: ", data.inb)
+    return 0
+
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((HOST, PORT))
+lsock.listen()
+print(("listening on", (HOST, PORT)))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+while True:
+    ret = 0
+    events = sel.select(timeout=None)
+    for key, mask in events:
+        if key.data is None:
+            accept_wrapper(key.fileobj)
+        else:
+            ret = service_connection(key, mask)
+    if ret < 0:
+        break
+
+sel.close()
