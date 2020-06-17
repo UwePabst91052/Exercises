1fff03fb6290dba1dfb8c7f795269e0dea1ff7ed
UwePabst91052 <upabst55@gmail.com>
2020-06-17 19:56:18 +0200

    Chat client and server with GUI support

--- Sockets/chat-client.py
+++ Sockets/chat-client.py
@@ -2,7 +2,7 @@ import socket
 import selectors
 import types
 import ChatClientGui
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 sel = selectors.DefaultSelector()
 
@@ -10,6 +10,11 @@ sel = selectors.DefaultSelector()
 class ChatClientWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(ChatClientWindow, self).__init__(parent)
+        self.ready_to_send = False
+        self.disconnect = False
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.monitor_selector)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect_server(self):
         global gsock
@@ -18,20 +23,21 @@ class ChatClientWindow(QtWidgets.QMainWindow):
         gsock = start_connection(host, int(port))
 
     def disconnect_server(self):
-        text = "exit"
-        events = sel.select(timeout=1)
-        if events:
-            for l_key, l_mask in events:
-                service_connection(l_key, l_mask, text)
-        sel.unregister(gsock)
-        sel.close()
+        self.disconnect = True
+        self.ready_to_send = True
 
     def send_message(self):
-        text = ui_window.messageEdit.toPlainText()
-        events = sel.select(timeout=1)
-        if events:
-            for l_key, l_mask in events:
-                service_connection(l_key, l_mask, text)
+        self.ready_to_send = True
+
+    def monitor_selector(self):
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            service_connection(key, mask)
+        if self.disconnect:
+            self.disconnect = False
+            self.timer.stop()
+            sel.unregister(gsock)
+            sel.close()
 
 
 def start_connection(host, port):
@@ -40,25 +46,40 @@ def start_connection(host, port):
     lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     lsock.setblocking(False)
     lsock.connect_ex(server_addr)
-    levents = selectors.EVENT_WRITE
+    levents = selectors.EVENT_READ | selectors.EVENT_WRITE
     data = types.SimpleNamespace(
         msg_total=0,
         message=None,
         outb=b"",
     )
     sel.register(lsock, levents, data=data)
+    ex.timer.start(500)
     return lsock
 
 
-def service_connection(key, mask, message):
-    lsock = key.fileobj
+def service_connection(key, mask):
+    sock = key.fileobj
     data = key.data
-    if mask & selectors.EVENT_WRITE:
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.inb = recv_data
+            message = data.inb.decode('utf-8').strip()
+            ui_window.receivedEdit.setPlainText(message)
+    if mask & selectors.EVENT_WRITE and ex.ready_to_send:
         if not data.outb:
-            data.outb = bytes(message, encoding='utf-8')
+            if not ex.disconnect:
+                message = ui_window.messageEdit.toPlainText()
+                data.outb = bytes(message, encoding='utf-8')
+            else:
+                data.outb = b"exit"
         if data.outb:
-            sent = lsock.send(data.outb)
+            sent = sock.send(data.outb)
             data.outb = data.outb[sent:]
+            ex.ready_to_send = len(data.outb) > 0
+            message = data.outb.decode('utf-8')
+            ui_window.messageEdit.setPlainText(message)
+
 
 gsock = None
 
@@ -68,3 +89,4 @@ ui_window = ChatClientGui.Ui_MainWindow()
 ui_window.setupUi(ex)
 ex.show()
 app.exec_()
+sel.close()
--- Sockets/chat-server.py
+++ Sockets/chat-server.py
@@ -1,22 +1,59 @@
 import socket
 import selectors
-import json
-import io
 import types
-
-HOST = "127.0.0.1"
-PORT = 65432
+from PyQt5 import QtWidgets, QtCore
+import ChatServerGui
 
 
 sel = selectors.DefaultSelector()
 
 
+class ChatServerWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(ChatServerWindow, self).__init__(parent)
+        self.ready_to_send = False
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.monitor_selector)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def await_connection(self):
+        host = ui_window.lineEdit.text()
+        port = int(ui_window.lineEdit_2.text())
+        self.sock.bind((host, port))
+        self.sock.listen()
+        status = "listening on" + str((host, port))
+        ui_window.statusLabel.setText(status)
+        self.sock.setblocking(False)
+        sel.register(self.sock, selectors.EVENT_READ, data=None)
+
+    def start_stop_monitoring(self, checked):
+        if checked:
+            ui_window.monitorButton.setText("Überwachung beenden")
+            self.timer.start(500)
+        else:
+            ui_window.monitorButton.setText("Überwachung starten")
+            ui_window.monitorButton.setChecked(False)
+            self.timer.stop()
+
+    def send_message(self):
+        self.ready_to_send = True
+
+    def monitor_selector(self):
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+
+
 def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read
-    print("accepted connection from", addr)
+    status = "accepted connection from {}".format(addr)
+    ui_window.statusLabel.setText(status)
     conn.setblocking(False)
     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
     sel.register(conn, events, data=data)
 
 
@@ -27,36 +64,32 @@ def service_connection(key, mask):
         recv_data = sock.recv(1024)
         if recv_data:
             data.inb = recv_data
-            message = data.inb.decode('utf-8').strip()
-            lines = message.split('\n')
-            for line in lines:
-                if line == "exit":
-                    print("closing connection to", data.addr)
-                    sel.unregister(sock)
-                    sock.close()
-                    return -1
-                else:
-                    print(line)
-    return 0
+            message = data.inb.decode('utf-8')
+            if message == "exit":
+                status = "closing connection to {}".format(data.addr)
+                ui_window.statusLabel.setText(status)
+                sel.unregister(sock)
+                sock.close()
+                ex.start_stop_monitoring(False)
+            else:
+                ui_window.receivedEdit.setPlainText(message)
+    if mask & selectors.EVENT_WRITE and ex.ready_to_send:
+        if not data.outb:
+            message = ui_window.messageEdit.toPlainText()
+            data.outb = bytes(message, encoding='utf-8')
+        if data.outb:
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+            ex.ready_to_send = len(data.outb) > 0
+            message = data.outb.decode('utf-8')
+            ui_window.messageEdit.setPlainText(message)
 
 
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((HOST, PORT))
-lsock.listen()
-print(("listening on", (HOST, PORT)))
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
-
-while True:
-    ret = 0
-    try:
-        events = sel.select(timeout=None)
-    except KeyboardInterrupt:
-        break
-    for key, mask in events:
-        if key.data is None:
-            accept_wrapper(key.fileobj)
-        else:
-            ret = service_connection(key, mask)
-
+app = QtWidgets.QApplication([])
+ex = ChatServerWindow()
+ui_window = ChatServerGui.Ui_MainWindow()
+ui_window.setupUi(ex)
+ex.await_connection()
+ex.show()
+app.exec_()
 sel.close()
