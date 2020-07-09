c110f2077d19e0c76760a70cbbe57998c6ecb4dc
UwePabst91052 <upabst55@gmail.com>
2020-07-09 19:13:36 +0200

    advanced chat client and server using protocol with json

---
+++ Sockets/app-client.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import sys
+import socket
+import selectors
+import traceback
+from PyQt5 import QtWidgets, QtCore
+
+import libclient
+import ChatClientGui
+
+sel = selectors.DefaultSelector()
+
+
+class ChatClientWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(ChatClientWindow, self).__init__(parent)
+        self.disconnect = False
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.monitor_selector)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect_server(self):
+        global gsock
+        host = ui_window.lineEdit.text()
+        port = int(ui_window.lineEdit_2.text())
+        gsock = start_connection(host, port)
+
+    def disconnect_server(self):
+        global message
+        self.disconnect = True
+        action = "disconnect"
+        message.request = create_request(action, "client")
+        message.ready_to_send = True
+        message.set_selector_events_mask("w")
+
+    def send_message(self):
+        global message
+        action = "send"
+        value = ui_window.messageEdit.toPlainText()
+        message.request = create_request(action, value)
+        message.ready_to_send = True
+        message.set_selector_events_mask("w")
+
+    def monitor_selector(self):
+        events = sel.select(timeout=None)
+        message = None
+        for key, mask in events:
+            message = key.data
+            try:
+                message.process_events(mask)
+            except Exception:
+                print(
+                    "main: error: exception for",
+                    f"{message.addr}:\n{traceback.format_exc()}",
+                )
+                message.close()
+        if self.disconnect:
+            self.disconnect = False
+            self.timer.stop()
+            if message is not None:
+                message.close()
+
+    def display_received_message(self, text):
+        ui_window.receivedEdit.setPlainText(text)
+
+
+def create_request(action, value):
+    if action in ("send", "login", "logout", "peer2peer", "disconnect"):
+        return dict(
+            type="text/json",
+            encoding="utf-8",
+            content=dict(action=action, value=value),
+        )
+    else:
+        return dict(
+            type="binary/custom-client-binary-type",
+            encoding="binary",
+            content=bytes(action + value, encoding="utf-8"),
+        )
+
+
+def start_connection(host, port):
+    global message
+    addr = (host, port)
+    print("starting connection to", addr)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(False)
+    sock.connect_ex(addr)
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    message = libclient.Message(sel, sock, addr, ex)
+    sel.register(sock, events, data=message)
+    ex.timer.start(500)
+    return sock
+
+
+gsock = None
+message = None
+
+app = QtWidgets.QApplication([])
+ex = ChatClientWindow()
+ui_window = ChatClientGui.Ui_MainWindow()
+ui_window.setupUi(ex)
+ex.show()
+app.exec_()
+sel.close()
+
---
+++ Sockets/app-server.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import sys
+import socket
+import selectors
+import traceback
+
+import libserver
+
+sel = selectors.DefaultSelector()
+
+
+def accept_wrapper(sock):
+    global client_list
+    conn, addr = sock.accept()  # Should be ready to read
+    print("accepted connection from", addr)
+    conn.setblocking(False)
+    message = libserver.Message(sel, conn, addr)
+    client_list.append((conn, addr, message))
+    message.client_list = client_list
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=message)
+
+
+if len(sys.argv) != 3:
+    host, port = '127.0.0.1', 65432
+else:
+    host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Avoid bind() exception: OSError: [Errno 48] Address already in use
+lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+lsock.bind((host, port))
+lsock.listen()
+print("listening on", (host, port))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+client_list = []
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                message = key.data
+                try:
+                    message.process_events(mask)
+                except Exception:
+                    print(
+                        "main: error: exception for",
+                        f"{message.addr}:\n{traceback.format_exc()}",
+                    )
+                    message.close()
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
